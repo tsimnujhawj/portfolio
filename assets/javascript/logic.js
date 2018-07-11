d05b0178470634c603cbfd9e17d7ea79f0ef5b2c
tsimnujhawj <tsimnuj.hawj@gmail.com>
2018-07-11 15:32:19 -0500

    added smooth scroll, error shows in console, but it still functions. added box shadowing to header

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,5 +1,31 @@
 $(document).ready(function() { // DOCUMENT READY OPENING
 
-
+// SMOOTH SCROLLING
+  
+    let scrollLink = $(".scroll");
+    
+    // Smooth scrolling
+    scrollLink.click(function(e) {
+      e.preventDefault();
+      $("body, html").animate({
+        scrollTop: $(this.hash).offset().top
+      }, 1000 );
+    });
+    
+    // Active link switching
+    $(window).scroll(function() {
+      let scrollbarLocation = $(this).scrollTop();
+      
+      scrollLink.each(function() {
+        
+        let sectionOffset = $(this.hash).offset().top;
+        
+        if (sectionOffset <= scrollbarLocation) {
+          $(this).parent().addClass("active");
+          $(this).parent().siblings().removeClass("active");
+        }
+      })
+      
+    })
     
 }); // DOCUMENT READY CLOSING
